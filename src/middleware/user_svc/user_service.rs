671a448fd77e2e3700b2149fc87e4a797e087269
Franklin <franklinblanco@tutanota.com>
2022-08-27 16:20:34 -0400

    Changed auth_user_with_token to only accept an object

--- src/middleware/user_svc/user_service.rs
+++ src/middleware/user_svc/user_service.rs
@@ -6,8 +6,9 @@ use crate::middleware::client::perform_request;
 //TODO: Move this into a separate service
 const BASE_URL_USER_SVC: &str = "http://backend.blancoinfante.com";
 
-pub async fn authenticate_user_with_token(client: &Client, user: &UserForAuthenticationDto, user_id: &i32) -> Result<User, Error> {
-    perform_request::<&UserForAuthenticationDto, User>(BASE_URL_USER_SVC.to_string(), client, Method::POST, format!("/user/auth/token/{}", user_id), Some(user), 200, vec![(String::from("auth-token"), user.token.clone())]).await
+pub async fn authenticate_user_with_token(client: &Client, user: &UserForAuthenticationDto) -> Result<User, Error> {
+    //TODO: Remove the need for a userdto, just get token and Id, dont send the body
+    perform_request::<&UserForAuthenticationDto, User>(BASE_URL_USER_SVC.to_string(), client, Method::POST, format!("/user/auth/token/{}", user.id), None, 200, vec![(String::from("auth-token"), user.token.clone())]).await
 }
 pub async fn create_user(client: &Client, user: &UserForCreationDto) -> Result<Token, Error> {
     perform_request::<&UserForCreationDto, Token>(BASE_URL_USER_SVC.to_string(), client, Method::POST, "/user".to_string(), Some(user), 200, vec![]).await
