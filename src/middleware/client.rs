a9b5f28d3e0daadc3e5c6c36a905bea9ebc05823
Franklin <franklinblanco@tutanota.com>
2022-09-08 13:06:53 -0400

    added sync method for perform_request_without_client

--- src/middleware/client.rs
+++ src/middleware/client.rs
@@ -106,3 +106,55 @@ pub async fn perform_request_without_client<B: Serialize, R: DeserializeOwned>(
         }
     }
 }
+/// Same as function above but blocking
+pub async fn perform_request_without_client_sync<B: Serialize, R: DeserializeOwned>(
+    base_url: String,
+    method: reqwest::Method,
+    path: String,
+    body: Option<B>,
+    expected_status_code: u16,
+    headers: Vec<(String, String)>,
+) -> Result<R, Error> {
+    let client = reqwest::blocking::Client::new();
+    let mut req_incomplete =
+        client.request(method, format!("{url}{path}", url = base_url, path = path));
+
+    for header in headers {
+        req_incomplete = req_incomplete.header(&header.0, &header.1);
+    }
+
+    let req_complete = match body {
+        Some(b) => req_incomplete.json(&b),
+        None => req_incomplete.header("content-length", 0),
+    };
+    println!("{:?}", req_complete);
+    match req_complete.send() {
+        // Error handling here
+        Ok(res) => {
+            // Request sent correctly
+            match res.status().as_u16() == expected_status_code {
+                true => {
+                    match res.json::<R>() {
+                        Ok(resp_dto) => Ok(resp_dto), //  Return correctly deserialized obj
+                        Err(err) => Err(Error::ClientError(MessageResource::new_from_err(err))),
+                    }
+                }
+                false => {
+                    //If status code is any other than expected
+                    Err(Error::UnexpectedStatusCode(
+                        expected_status_code,
+                        res.status().as_u16(),
+                        match res.json::<Vec<MessageResource>>() {
+                            Ok(messages) => messages,
+                            Err(e) => vec![MessageResource::new_from_err(e.to_string())],
+                        },
+                    ))
+                }
+            }
+        }
+        Err(e) => {
+            //  Request couldn't be sent
+            Err(Error::ClientError(MessageResource::new_from_err(e)))
+        }
+    }
+}
